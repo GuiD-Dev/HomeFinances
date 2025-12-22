35351fbc5efd000296c96aa8879be786b100d8d2
Guilherme <guilhermed.dev@gmail.com>
2025-12-22 18:35:26 -0300

    refactor: include join between person and transactions on personRepository

--- GastosResidenciais.WebApi/GastosResidenciais.WebAPI.Infra/Repositories/PersonRepository.cs
+++ GastosResidenciais.WebApi/GastosResidenciais.WebAPI.Infra/Repositories/PersonRepository.cs
@@ -1,6 +1,7 @@
 using GastosResidenciais.WebApi.Application.Interfaces;
 using GastosResidenciais.WebApi.Domain.Entities;
 using GastosResidenciais.WebApi.Infra.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace GastosResidenciais.WebApi.Infra.Repositories;
 
@@ -8,7 +9,7 @@ public class PersonRepository(PgSqlDbContext context) : IPersonRepository
 {
     public IEnumerable<Person> GetMany()
     {
-        return context.People.ToList();
+        return context.People.Include(p => p.Transactions).ToList();
     }
 
     public Person GetOneById(int id)
