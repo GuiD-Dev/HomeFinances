631042d976869a065b55d9e3b5d4a0729ea965ae
Guilherme <guilhermed.dev@gmail.com>
2026-01-17 14:17:52 -0300

    feat(Domain/DomainException.cs): add a class to represent a domain violation rule

--- HomeFinances.WebApi/HomeFinances.WebApi.Domain/Exceptions/DomainException.cs
+++ HomeFinances.WebApi/HomeFinances.WebApi.Domain/Exceptions/DomainException.cs
@@ -1,3 +1,15 @@
 namespace HomeFinances.WebApi.Domain.Exceptions;
 
-public class DomainException(string message) : Exception(message) { }
+public class DomainException(string message) : Exception(message)
+{
+  public static void ThrowsWhen(List<(bool condition, string message)> validations)
+  {
+    var errors = new List<string>();
+
+    foreach (var validation in validations)
+      if (validation.condition) errors.Add(validation.message);
+
+    if (errors.Any())
+      throw new DomainException(string.Join(" | ", errors));
+  }
+}
